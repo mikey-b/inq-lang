38053d5ba2918f6ccdf751017a24656914b2f7a3
Michael Brown <mikey.be@gmail.com>
2018-11-23 22:17:32 +0000

    Added websocket to project
    Basic websocket support added

--- source/htppobject.d
+++ source/htppobject.d
@@ -4,6 +4,41 @@ import inqobject;
 import std.json;
 import vibe.d;
 
+final class WebInterface {
+	string[] messages;
+	LocalManualEvent messageEvent;
+	
+	this()
+	{
+		messageEvent = createManualEvent();
+	}
+	
+	void get() {
+	
+	}
+	void getWS(scope WebSocket socket) {
+		messages ~= "New Connection";
+	
+		auto writer = runTask({
+			auto nm = messages.length;
+			
+			while(socket.connected) {
+				while (nm < messages.length) socket.send(messages[nm++]);
+				while (messages.length <= nm) messageEvent.wait();
+			}
+		});
+		
+		while (socket.waitForData) {
+			auto message = socket.receiveText();
+			if (message.length) messages ~= message; 
+			messageEvent.emit();
+		}
+		
+		writer.join();
+	}
+}
+		
+
 class HTTPObject: physical_object {
 	
 	this(string id) {
@@ -14,14 +49,16 @@ class HTTPObject: physical_object {
 	class HTTPController: DefaultController {
 		override void start() {
 			spawn((){
+				auto router = new URLRouter;
+				
+				router.registerWebInterface(new WebInterface);
+				router.get("*", serveStaticFiles("html/"));
+								
 				auto settings = new HTTPServerSettings;
 				settings.port = 8080;
+				//settings.options &= ~HTTPServerOption.errorStackTraces;
 				
-				auto l = listenHTTP(settings, 
-					(HTTPServerRequest req, HTTPServerResponse res)
-					{
-						res.writeBody(cast(ubyte[])"Inq web interface", "text/plain");
-					});
+				auto l = listenHTTP(settings, router);
 				scope (exit) l.stopListening();
 
 				runApplication();
